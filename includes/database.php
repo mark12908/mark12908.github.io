b954d2f650eee125ec26e0c0cc30c91514af8262
mark12908 <eloghoaken@gmail.com>
2022-06-09 17:15:00 +1000

    test

--- includes/database.php
+++
@@ -1,9 +0,0 @@
-<?php
-$con= new mysqli('localhost','root','','attempts')or die("Could not connect to mysql".mysqli_error($con));
-
-?>
-
-<?php
-$con= new mysqli('localhost','root','','attempts')or die("Could not connect to mysql".mysqli_error($con));
-
-?>
--- logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-	session_start(); 
-	session_destroy(); // Destroy the session itself.
-	setcookie ("mark",  "",  time() - 3600);// Destroy the user name cookie	
-	header("Location: login.php");
-	
-?>
